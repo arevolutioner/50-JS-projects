ad38abdcf2ccba31b66f0692044d8a3234b5d159
Your Name <andrey.andonov@yahoo.com>
2021-01-23 20:30:12 +0200

    update

--- Eloquent JS/programStructure.js
+++ Eloquent JS/programStructure.js
@@ -1554,4 +1554,58 @@ You have to use the arguments object.
   
 //   addTogether(2,3);
 
+// const Person = function(firstAndLast) {
+//     var fullName = firstAndLast;
+  
+//     this.getFirstName = function() {
+//       return fullName.split(" ")[0];
+//     };
+  
+//     this.getLastName = function() {
+//       return fullName.split(" ")[1];
+//     };
+  
+//     this.getFullName = function() {
+//       return fullName;
+//     };
+  
+//     this.setFirstName = function(name) {
+//       fullName = name + " " + fullName.split(" ")[1];
+//     };
+  
+//     this.setLastName = function(name) {
+//       fullName = fullName.split(" ")[0] + " " + name;
+//     };
+  
+//     this.setFullName = function(name) {
+//       fullName = name;
+//     };
+//   };
+  
+//   var bob = new Person("Bob Ross");
+//   bob.getFullName();
 
+// function orbitalPeriod(arr) {
+//     var GM = 398600.4418;
+//     var earthRadius = 6367.4447;
+//     var a = 2 * Math.PI;
+//     var newArr = [];
+  
+//     var getOrbPeriod = function(obj) {
+//       var c = Math.pow(earthRadius + obj.avgAlt, 3);
+//       var b = Math.sqrt(c / GM);
+//       var orbPeriod = Math.round(a * b);
+//       // create new object
+//       return {name: obj.name, orbitalPeriod: orbPeriod};
+//     };
+  
+//     for (var elem in arr) {
+//       newArr.push(getOrbPeriod(arr[elem]));
+//     }
+  
+//     return newArr;
+    
+//   }
+  
+//   orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+ 
